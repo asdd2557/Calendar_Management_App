97a0b359515d2db5d897bfd11380d80d7046d628
asdd2557 <asdd2557@naver.com>
2024-12-10 11:38:58 +0900

    second push

--- src/main/java/org/example/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.Schedule;
-import org.example.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
